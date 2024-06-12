31b89f49549518fbb58ff78bd8ffe40bf49c0abb
Matthew Hillman <mhillman2@aol.com>
2024-06-12 21:49:25 +0100

    Increase size of primes demo

--- demo/PrimesDemo.hpp
+++ demo/PrimesDemo.hpp
@@ -7,12 +7,13 @@ class PrimeHashDefinition {
   using TKey = int;
 
   // Some sample values we got from a search
-  constexpr static std::size_t factor = 9239361228047464788ULL;
-  constexpr static std::size_t shift = 55;
-  constexpr static std::size_t bits = 4;
+  constexpr static std::size_t factor = 1866174325241492882;
+  constexpr static std::size_t shift = 23;
+  constexpr static std::size_t bits = 5;
 
   static std::size_t baseHash(TKey key) { return std::hash<int>{}(key); }
 
-  static constexpr std::array<int, 10> keySet{
-      2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+  static constexpr std::array<int, 20> keySet{2,  3,  5,  7,  11, 13, 17,
+                                              19, 23, 29, 31, 37, 41, 43,
+                                              47, 53, 59, 61, 67, 71};
 };
